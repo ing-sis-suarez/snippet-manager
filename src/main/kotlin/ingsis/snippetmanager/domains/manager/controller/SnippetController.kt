c57fdfbfea5442579b7bfacd5a001c6817af030d
Jorge <jorge.sv.primero@gmail.com>
2023-06-19 20:21:16 -0300

    a

--- src/main/kotlin/ingsis/snippetmanager/domains/manager/controller/SnippetController.kt
+++ src/main/kotlin/ingsis/snippetmanager/domains/manager/controller/SnippetController.kt
@@ -1,11 +1,8 @@
 package ingsis.snippetmanager.domains.manager.controller
 
-import ingsis.snippetmanager.domains.manager.dto.SnippetDTO
-import ingsis.snippetmanager.domains.manager.model.Snippet
 import ingsis.snippetmanager.domains.manager.service.SnippetService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
-import java.security.Principal
 import java.util.*
 
 @RestController
--- src/main/kotlin/ingsis/snippetmanager/domains/manager/repository/ShareRepository.kt
+++ src/main/kotlin/ingsis/snippetmanager/domains/manager/repository/ShareRepository.kt
@@ -2,10 +2,9 @@ package ingsis.snippetmanager.domains.manager.repository
 
 import ingsis.snippetmanager.domains.manager.model.Snippet
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import java.util.*
 
-interface ShareRepository: JpaRepository<Snippet, UUID>{
+interface ShareRepository : JpaRepository<Snippet, UUID> {
 
-   override fun findById(id: UUID): Optional<Snippet>
+    override fun findById(id: UUID): Optional<Snippet>
 }
